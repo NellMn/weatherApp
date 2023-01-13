4740fe67e061bbe5b35e6f7090bf4f324b80e44a
NellMn <118570134+NellMn@users.noreply.github.com>
2023-01-13 13:11:17 +0330

    adding forecast

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -52,9 +52,10 @@ function displayForecast() {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "258213fabfb6e561af7eeb257d2a3047";
+  let apiKey = "c6f8ef4575250284954db9f4dfa7a996";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -84,7 +85,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "258213fabfb6e561af7eeb257d2a3047";
+  let apiKey = "c6f8ef4575250284954db9f4dfa7a996";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -124,4 +125,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
